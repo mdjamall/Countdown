9509436c49607bcf37b32229c453b61d11699c08
mdjamall <mdjamalansari45@gmail.com>
2023-03-29 12:15:07 +0530

    first commit

---
+++ js/App.js
@@ -0,0 +1,30 @@
+const endDate="24 February 2023 10:00 PM";
+
+document.getElementById('end-date').innerText=endDate;
+const inputs = document.querySelectorAll("input");
+// const clock = ()=>{
+
+// }
+
+function clock(){
+    const end = new Date(endDate)
+    const now = new Date()
+    // console.log(end,now);
+    const diff = (end - now)/1000; //convert milliseconds into seconds
+    console.log(diff);
+    if(diff<0) return;
+    inputs[0].value = Math.floor(diff / 3600 / 24);
+    inputs[1].value = Math.floor(diff/3600) % 24;
+    inputs[2].value = Math.floor(diff/60) % 60;
+    inputs[3].value = Math.floor(diff)%60;
+}
+
+clock();
+
+
+setInterval(
+        () => {
+            clock()
+        },
+        1000
+)
